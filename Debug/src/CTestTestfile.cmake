ed45e99beaea2e54707e85e684e5a2742bb05b65
Gerard Wypych <science.artificer@gmail.com>
2017-11-13 02:16:10 +0100

    Deleting unnecessary files
    Only code files will be available

--- Debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sciart/Desktop/Liniowe/src
-# Build directory: /home/sciart/Desktop/Liniowe/Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
